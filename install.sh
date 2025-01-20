6dcbb448f8c0daee9c19f60d98aea6cb8fde7ca8
aarzaak <139879547+aarzaak@users.noreply.github.com>
2025-01-21 00:16:03 +0100

    new kernel version available

--- install.sh
+++ install.sh
@@ -11,11 +11,11 @@ sudo nala install -y make flex bison libssl-dev gcc-aarch64-linux-gnu g++-aarch6
 
 #http://ubuntu.univ-reims.fr/ubuntu/
 
-wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.6.71.tar.xz
+wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.6.72.tar.xz
 
-tar -xJvf linux-6.6.71.tar.xz
+tar -xJvf linux-6.6.72.tar.xz
 
-cd linux-6.6.71
+cd linux-6.6.72
 
 ARCH=arm64 CROSS_COMPILE=/bin/aarch64-linux-gnu- make defconfig
 ARCH=arm64 CROSS_COMPILE=/bin/aarch64-linux-gnu- make kvm_guest.config
