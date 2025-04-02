b8d3ff41c01db141b370b97d3d684b336ed5e3ee
aarzaak <139879547+aarzaak@users.noreply.github.com>
2025-04-02 10:28:20 +0200

    removed command to start vm from install.sh and put it in startvm.sh

--- install.sh
+++ install.sh
@@ -70,30 +70,3 @@ sudo virt-resize --expand /dev/sda2 2024-11-19-raspios-bookworm-armhf-lite.img 2
 cp 2024-11-19-raspios-bookworm-armhf-lite-bigger.img 2024-11-19-raspios-bookworm-armhf-lite-bigger.img.bak
 
 #cp 2024-11-19-raspios-bookworm-arm64-bigger.img 2024-11-19-raspios-bookworm-arm64-bigger.img.bak
-
-qemu-system-aarch64 \
--machine virt \
--cpu cortex-a76 \
--smp 4 \
--m 1G \
--kernel Image \
--append "root=/dev/vda2 rootfstype=ext4 rw panic=0 console=ttyAMA0" \
--drive format=raw,file=2024-11-19-raspios-bookworm-armhf-lite-bigger.img,if=none,id=hd0,cache=writeback \
--device virtio-blk,drive=hd0,bootindex=0 \
--netdev user,id=mynet,hostfwd=tcp::2222-:22 \
--device virtio-net-pci,netdev=mynet \
--monitor telnet:127.0.0.1:5555,server,nowait
-
-#qemu-system-aarch64 \
-#-machine virt \
-#-cpu cortex-a76 \
-#-smp 4 \
-#-m 1G \
-#-kernel Image \
-#-append "root=/dev/vda2 rootfstype=ext4 rw panic=0 console=ttyAMA0" \
-#-drive format=raw,file=2024-11-19-raspios-bookworm-arm64-bigger.img,if=none,id=hd0,cache=writeback \
-#-device virtio-blk,drive=hd0,bootindex=0 \
-#-netdev user,id=mynet,hostfwd=tcp::2222-:22 \
-#-device virtio-net-pci,netdev=mynet \
-#-monitor telnet:127.0.0.1:5555,server,nowait \
-#-nographic
