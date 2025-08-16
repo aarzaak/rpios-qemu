8f2510539c777265f78a4b90ee2ef60c52eb5601
aarzaak <139879547+aarzaak@users.noreply.github.com>
2025-08-16 10:14:54 +0200

    added a few messages to display

--- install.sh
+++ install.sh
@@ -3,33 +3,32 @@
 . ./color.sh
 
 if ! ./get-verified-tarball.sh; then
-    printf "${RED}Failed to retrieve kernel.${NS}\n"
+    printf "${RED}Failed to retrieve kernel. Exiting.${NS}\n"
     exit 1
 fi
 
 printf "${BLUE}Fetching updates…${NS}\n"
 sudo apt update
-printf "${GREEN}Updates have been fetched.${NS}\n"
+printf "${GREEN}Updates have been fetched.${NS}\n\n"
 
 printf "${BLUE}Updating system…${NS}\n"
 sudo apt full-upgrade -y
-printf "${GREEN}System has been updated.${NS}\n"
+printf "${GREEN}System has been updated.${NS}\n\n"
 
 printf "${BLUE}Installing required software…${NS}\n"
 sudo apt install -y curl gpg gpgv make flex bison libssl-dev gcc-aarch64-linux-gnu g++-aarch64-linux-gnu qemubuilder qemu-system-gui qemu-system-arm qemu-utils qemu-system-data qemu-system guestfs-tools
-printf "${GREEN}Required software has been installed.${NS}\n"
+printf "${GREEN}Required software has been installed.${NS}\n\n"
 
 printf "${BLUE}Decompressing kernel archive…${NS}\n"
 KERNEL="./.kernel"
 # Do we already have this file?
 if ! [[ -f ${KERNEL} ]]; then
-    echo "Kernel version not found because .kernel file does not exist."
+    printf "${RED}Kernel version not found because .kernel file does not exist. Exiting.${NS}\n\n"
     exit 1
 fi
 KERNELFILENAME=$(head -n 1 ${KERNEL})
-
 tar -xJf ${KERNELFILENAME}
-printf "${GREEN}Kernel archive has been decompressed.${NS}\n"
+printf "${GREEN}Kernel archive has been decompressed.${NS}\n\n"
 
 KERNELVERSION=${KERNELFILENAME%.*.*}
 
@@ -39,34 +38,31 @@ printf "${BLUE}Compiling kernel, this may take a few minutes…${NS}\n"
 ARCH=arm64 CROSS_COMPILE=/bin/aarch64-linux-gnu- make defconfig
 ARCH=arm64 CROSS_COMPILE=/bin/aarch64-linux-gnu- make kvm_guest.config
 ARCH=arm64 CROSS_COMPILE=/bin/aarch64-linux-gnu- make -j$(nproc)
-printf "${GREEN}Linux kernel has been compiled.${NS}\n"
+printf "${GREEN}Linux kernel has been compiled.${NS}\n\n"
 
 printf "${BLUE}Moving compiled kernel to parent folder…${NS}\n"
 mv arch/arm64/boot/Image ..
-printf "${GREEN}Compiled kernel has been moved to parent folder.${NS}\n"
+printf "${GREEN}Compiled kernel has been moved to parent folder.${NS}\n\n"
 
 cd ..
 
 printf "${BLUE}Downloading latest version of Raspberry Pi OS…${NS}\n"
 wget https://downloads.raspberrypi.com/raspios_lite_armhf/images/raspios_lite_armhf-2025-05-13/2025-05-13-raspios-bookworm-armhf-lite.img.xz
-printf "${GREEN}Latest version of Raspberry Pi OS has been downloaded.${NS}\n"
+printf "${GREEN}Latest version of Raspberry Pi OS has been downloaded.${NS}\n\n"
 
-xz -dk 2025-05-13-raspios-bookworm-armhf-lite.img.xz
-
-#wget https://downloads.raspberrypi.com/raspios_arm64/images/raspios_arm64-2024-11-19/2024-11-19-raspios-bookworm-arm64.img.xz
-
-#xz -dk 2024-11-19-raspios-bookworm-arm64.img.xz
-
-#openssl passwd -6
-#$6$JunV9CZDfUSGlapS$t5LCnmrsrNYdt0jTZNWVkdmao1XXy.DIo.62v2CGPIJq73vuAd8JhnhN9zmE82AtCN/gpAoBasNtAQ9Y08wuH/
+xz -d 2025-05-13-raspios-bookworm-armhf-lite.img.xz
 
 sudo mount -o loop,offset=$((512*1064960)) 2025-05-13-raspios-bookworm-armhf-lite.img /mnt
 
-#sudo mount -o loop,offset=4194304 2024-11-19-raspios-bookworm-arm64.img /mnt
-
+printf "${BLUE}Defining password for pi user…${NS}\n"
+#openssl passwd -6
+#$6$JunV9CZDfUSGlapS$t5LCnmrsrNYdt0jTZNWVkdmao1XXy.DIo.62v2CGPIJq73vuAd8JhnhN9zmE82AtCN/gpAoBasNtAQ9Y08wuH/
 echo 'pi:$6$JunV9CZDfUSGlapS$t5LCnmrsrNYdt0jTZNWVkdmao1XXy.DIo.62v2CGPIJq73vuAd8JhnhN9zmE82AtCN/gpAoBasNtAQ9Y08wuH/' | sudo tee /mnt/userconf.txt
+printf "${GREEN}Password for pi user has been defined.${NS}\n\n"
 
+printf "${BLUE}Enabling SSH server…${NS}\n"
 sudo touch /mnt/ssh
+printf "${GREEN}SSH server has been enabled.${NS}\n\n"
 
 sudo umount /mnt
 
@@ -74,10 +70,7 @@ cp 2025-05-13-raspios-bookworm-armhf-lite.img 2025-05-13-raspios-bookworm-armhf-
 truncate -s 16G 2025-05-13-raspios-bookworm-armhf-lite-bigger.img
 sudo virt-resize --expand /dev/sda2 2025-05-13-raspios-bookworm-armhf-lite.img 2025-05-13-raspios-bookworm-armhf-lite-bigger.img
 
-#cp 2024-11-19-raspios-bookworm-arm64.img 2024-11-19-raspios-bookworm-arm64-bigger.img
-#truncate -s 16G 2024-11-19-raspios-bookworm-arm64-bigger.img
-#sudo virt-resize --expand /dev/sda2 2024-11-19-raspios-bookworm-arm64.img 2024-11-19-raspios-bookworm-arm64-bigger.img
+rm 2025-05-13-raspios-bookworm-armhf-lite.img
 
 cp 2025-05-13-raspios-bookworm-armhf-lite-bigger.img 2025-05-13-raspios-bookworm-armhf-lite-bigger.img.bak
 
-#cp 2024-11-19-raspios-bookworm-arm64-bigger.img 2024-11-19-raspios-bookworm-arm64-bigger.img.bak
